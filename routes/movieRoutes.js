a4dc0ea2742ac847452829fd0a3a08b0a257c471
DawnBreaker <tunganhngo207@gmail.com>
2024-04-11 16:07:14 +0700

    Update: pagination

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import Movie from '../models/films.js';
-import movies from '../movies.json' assert { type: 'json' };
+// import movies from '../movies.json' assert { type: 'json' };
 const movieRoute = Router();
 
 movieRoute.get('/', async (req, res) => {
@@ -61,7 +61,30 @@ movieRoute.get('/', async (req, res) => {
     res.status(500).json(error);
   }
 });
+movieRoute.get('/', async (req, res) => {
+  try {
+    // We destructure the req.query object to get the page and limit variables from url
+    const { page = 1, limit = 10 } = req.query;
+    const movies = await Movie.find({ ...req.query })
+      // We multiply the "limit" variables by one just to make sure we pass a number and not a string
+      .limit(limit * 1)
+      // I don't think i need to explain the math here
+      .skip((page - 1) * limit)
+      // We sort the data by the date of their creation in descending order (user 1 instead of -1 to get ascending order)
+      .sort({ createdAt: -1 });
 
+    // Getting the numbers of products stored in database
+    const count = await Movie.countDocuments();
+
+    return res.status(200).json({
+      movies,
+      totalPages: Math.ceil(count / limit),
+      currentPage: page,
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 // const insertMovies = async () => {
 //   try {
 //     const docs = await Movie.insertMany(movies);
